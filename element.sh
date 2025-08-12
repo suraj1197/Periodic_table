4a4927ca95bd613d330c23178b27b62502bc05ea
suraj1197 <surajmalyan11@gmail.com>
2025-08-12 08:22:16 -0400

    refactor: modifying the output

--- element.sh
+++ element.sh
@@ -2,12 +2,14 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo "Please provide an element as an argument."
 
+argument=$1 # taking the first argument 
+
 TABLE=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass,p.melting_point_celsius,p.boiling_point_celsius from elements e JOIN properties p ON e.atomic_number= p.atomic_number JOIN types t ON p.type_id= t.type_id
-          WHERE e.atomic_number=1")
+          WHERE e.atomic_number= $1")
 
 IFS="|" read atomic_number name symbol type atomic_mass metling_point_celcius boiling_point_celsius <<< "$TABLE"
 
-echo "NAME: $name"
+echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $metling_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
 
 
 
