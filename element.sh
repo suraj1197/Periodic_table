3ba08d87728532066e54c1b137260bda78cf5c55
suraj1197 <surajmalyan11@gmail.com>
2025-08-12 08:44:34 -0400

    fix: element.sh is fixed and completed

--- element.sh
+++ element.sh
@@ -17,12 +17,15 @@ TABLE=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass,p
           WHERE e.symbol= '$argument' OR e.name='$argument'")
 fi
 
-
+if [[ -z $TABLE ]]
+then 
+  echo "I could not find that element in the database."
+else
 
 IFS="|" read atomic_number name symbol type atomic_mass metling_point_celcius boiling_point_celsius <<< "$TABLE"
 
 echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $metling_point_celcius celsius and a boiling point of $boiling_point_celsius celsius."
-
+fi
 
 
 
